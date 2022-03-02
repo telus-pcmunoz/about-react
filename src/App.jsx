4c52414e2930c1042a2f0485161da7b98980199b
Patrick Ceasar Muñoz <87862197+telus-pcmunoz@users.noreply.github.com>
2022-03-02 22:57:18 +0800

    Add all components to App

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,28 @@
 import React from "react";
 import SimpleComponent from "./SimpleComponent";
+import FunctionalComponent from "./FunctionalComponent";
+import ClassComponent from "./ClassComponent";
+import ControlledComponent from "./ControlledComponent";
+import CompleteLifecycleCompoenent from "./CompleteLifecycleCompoenent";
 
 function App() {
   return (
     <>
-      <SimpleComponent />
+      <div>
+        <SimpleComponent />
+      </div>
+      <div>
+        <FunctionalComponent />
+      </div>
+      <div>
+        <ClassComponent />
+      </div>
+      <div>
+        <ControlledComponent />
+      </div>
+      <div>
+        <CompleteLifecycleCompoenent />
+      </div>
     </>
   );
 }
